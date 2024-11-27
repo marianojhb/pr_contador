a8bf93ee2b409d59cb76d8daedb1aba337c27e3a
Mariano Belgrano <mbelgrano@gmail.com>
2024-11-27 10:38:00 -0300

    contador simple

---
+++ src/App.svelte
@@ -0,0 +1,69 @@
+<script>
+  import Button from './shared/Button.svelte'
+  let value = 0
+  let flasheffect
+  let presionado = null
+
+  function handleClick(param, buttonId) {
+    presionado = buttonId
+    value = value + param
+
+    setTimeout(() => {
+      presionado = null
+      flasheffect = true
+    }, 10)
+  }
+</script>
+
+<main>
+  <h1>Simple counter</h1>
+  <Button on:click={() => handleClick(1, 'up')} flasheffect={presionado == 'up'}
+    >Up</Button
+  >
+
+  <div class="number" class:positive={value > 0} class:negative={value < 0}>
+    {value}
+  </div>
+
+  <Button
+    on:click={() => handleClick(-1, 'down')}
+    flasheffect={presionado == 'down'}>Down</Button
+  >
+</main>
+
+<style>
+  main {
+    text-align: center;
+    max-width: 960px;
+    margin: 0 auto;
+    background-color: white;
+    padding: 10px 0;
+    height: 100%;
+  }
+
+  h1 {
+    height: 100px;
+  }
+
+  .number {
+    display: flex;
+    font: 48px sans-serif;
+    height: 150px;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .positive {
+    color: green;
+  }
+
+  .negative {
+    color: red;
+  }
+
+  @media (max-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
---
+++ src/shared/Button.svelte
@@ -0,0 +1,49 @@
+<script>
+  export let flasheffect
+</script>
+
+<button on:click class:flasheffect>
+  <slot></slot>
+</button>
+
+<style>
+  button {
+    box-sizing: border-box;
+    padding: 0px;
+    margin: 0px;
+    width: 150px;
+    height: 150px;
+    background-color: #ddd;
+    cursor: pointer;
+  }
+
+  button:hover {
+    color: white;
+    border: solid 1px black;
+  }
+
+  button:not(:hover) {
+    border: solid 1px #cbcdcb;
+  }
+
+  .flasheffect {
+    animation-name: flasheffect;
+    animation-duration: 0.1s;
+  }
+
+  @keyframes flasheffect {
+    0% {
+      opacity: 0;
+    }
+    25% {
+      opacity: 0.25;
+    }
+    75% {
+      opacity: 0.7;
+    }
+    100% {
+      opacity: 0.99;
+    }
+  }
+
+</style>
